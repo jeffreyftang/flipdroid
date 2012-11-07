86e776a3bd2853078ea029912bfe2e801236c077
Jeffrey Tang <jeffreyftang@gmail.com>
2012-11-07 13:27:32 -0600

    Show action bar when long press on deck item

--- src/com/bonkler/flipdroid/MainActivity.java
+++ src/com/bonkler/flipdroid/MainActivity.java
@@ -3,7 +3,7 @@ package com.bonkler.flipdroid;
 import android.app.Activity;
 import android.os.Bundle;
 
-import android.support.v4.app.FragmentActivity;
+// import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
@@ -13,18 +13,29 @@ import android.support.v4.widget.*;
 import android.database.Cursor;
 import android.content.Intent;
 import android.view.View;
-import android.view.MenuItem;
-import android.view.MenuInflater;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.widget.ListView;
-import android.widget.AdapterView.AdapterContextMenuInfo;
 
-public class MainActivity extends FragmentActivity
+// import android.view.ContextMenu;
+// import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+// import android.widget.AdapterView.AdapterContextMenuInfo;
+
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.ActionMode;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.app.SherlockListFragment;
+
+public class MainActivity extends SherlockFragmentActivity
 {
     public final static String CLICKED_DECK_NAME = "com.bonkler.flipdroid.DECK_NAME";
     public final static String CLICKED_DECK_ID = "com.bonkler.flipdroid.DECK_ID";
     public final static String CLICKED_DECK_CARD_IDS = "com.bonkler.flipdroid.CARD_IDS";
+
+    
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,11 +51,13 @@ public class MainActivity extends FragmentActivity
     }
 
     // A fragment that will hold the list of FlipDecks
-    public static class DeckListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    public static class DeckListFragment extends SherlockListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
         private FlipCursorLoader mLoader;
         private SimpleCursorAdapter mAdapter;
         private static FlipDeck activeDeck;
+
+        private ActionMode mActionMode;
 
         String[] fromColumns = {FlipDroidContract.MyDecks.COLUMN_DECK_NAME};
         int[] toViews = {R.id.text};
@@ -54,17 +67,63 @@ public class MainActivity extends FragmentActivity
             super.onActivityCreated(savedInstanceState);
 
             // Initialize an adapter with a null cursor. We will update this cursor in the onLoadFinished() callback.
-            mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.deck_item, null, fromColumns, toViews, 0);
+            mAdapter = new SimpleCursorAdapter(getSherlockActivity(), R.layout.deck_item, null, fromColumns, toViews, 0);
             setEmptyText("No decks found.");
             setListAdapter(mAdapter);
             setListShownNoAnimation(false); // Don't display a loading animation.
 
             // Initialize the loader, using this fragment as the callback object.
-            mLoader = (FlipCursorLoader) getActivity().getSupportLoaderManager().initLoader(1, null, this);
+            mLoader = (FlipCursorLoader) getSherlockActivity().getSupportLoaderManager().initLoader(1, null, this);
 
             // Enable context menu on long press for each list item.
-            registerForContextMenu(getListView());
+            // registerForContextMenu(getListView());
+
+            getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView parent, View view, int position, long id) {
+                    mActionMode = getSherlockActivity().startActionMode(mActionModeCallback);
+                    return true;
+                }
+            });
         }
+
+        private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+            // Called when the action mode is created; startActionMode() was called
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                // Inflate a menu resource providing context menu items
+                MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.deck_context_menu, menu);
+                return true;
+            }
+
+            // Called each time the action mode is shown. Always called after onCreateActionMode, but
+            // may be called multiple times if the mode is invalidated.
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false; // Return false if nothing is done
+            }
+
+            // Called when the user selects a contextual menu item
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                // switch (item.getItemId()) {
+                //     case R.id.menu_share:
+                //         shareCurrentItem();
+                //         mode.finish(); // Action picked, so close the CAB
+                //         return true;
+                //     default:
+                //         return false;
+                // }
+                return true;
+            }
+
+            // Called when the user exits the action mode
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                mActionMode = null;
+            }
+        };
 
         @Override
         public void onListItemClick(ListView I, View v, int position, long id) {
@@ -87,29 +146,29 @@ public class MainActivity extends FragmentActivity
             // intent.putExtra(CLICKED_DECK_CARD_IDS, cardIds);
             // intent.putExtra(CLICKED_DECK_NAME, theName);
 
-            Intent intent = new Intent(getActivity(), BrowseDeckActivity.class);
+            Intent intent = new Intent(getSherlockActivity(), BrowseDeckActivity.class);
             startActivity(intent);
         }
 
-        @Override
-        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-            super.onCreateContextMenu(menu, v, menuInfo);
-            MenuInflater inflater = getActivity().getMenuInflater();
-            inflater.inflate(R.menu.deck_context_menu, menu);
-        }
+        // @Override
+        // public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        //     super.onCreateContextMenu(menu, v, menuInfo);
+        //     MenuInflater inflater = getActivity().getMenuInflater();
+        //     inflater.inflate(R.menu.deck_context_menu, menu);
+        // }
 
-        @Override
-        public boolean onContextItemSelected(MenuItem item) {
-            AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-            activeDeck = new FlipDeck(mAdapter.getCursor(), info.position);
-            switch (item.getItemId()) {
-                case R.id.edit_deck_option:
-                    startEditDeck();
-                    return true;
-                default:
-                    return super.onContextItemSelected(item);
-            }
-        }
+        // @Override
+        // public boolean onContextItemSelected(MenuItem item) {
+        //     AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+        //     activeDeck = new FlipDeck(mAdapter.getCursor(), info.position);
+        //     switch (item.getItemId()) {
+        //         case R.id.edit_deck_option:
+        //             startEditDeck();
+        //             return true;
+        //         default:
+        //             return super.onContextItemSelected(item);
+        //     }
+        // }
 
         private void startEditDeck() {
             //Intent intent = new Intent(getActivity(), EditDeckActivity.class);
